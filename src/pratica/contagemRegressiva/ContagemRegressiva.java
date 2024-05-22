1411ed7c99b299e58075bcb4f4c6ac5ba9ddd7ae
Wagner <wagnergoulart0@gmail.com>
2024-05-22 20:36:16 -0300

    ADD: exercicios: Contagem Regressiva / Multiplos de 3
    Signed-off-by: Wagner <wagnergoulart0@gmail.com>

---
+++ src/pratica/contagemRegressiva/ContagemRegressiva.java
@@ -0,0 +1,11 @@
+package pratica.contagemRegressiva;
+
+public class ContagemRegressiva {
+    public static void main(String[] args) {
+        int inicio = 10;
+
+        for(int i = inicio; i >= 0; i--){
+            System.out.println(i);
+        }
+    }
+}
---
+++ src/pratica/multiplosDe3/MultiplosDe3.java
@@ -0,0 +1,12 @@
+package pratica.multiplosDe3;
+
+public class MultiplosDe3 {
+    public static void main(String[] args) {
+        int numeroAlvo = 100;
+        for (int i = 1 ; i <= numeroAlvo; i++){
+            if (i % 3 ==0) {
+                System.out.println(i);
+            }
+        }
+    }
+}
